1f64df075c4f6c40506ea98257f1dc7f63b18964
woojh <woojh3690@gmail.com>
2025-08-13 00:16:47 +0900

    리팩토링: 아이콘 경로 및 소스코드 미병합 처리

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const toggleCheckbox = document.getElementById('previewToggle');
 
 // Load current state
@@ -22,28 +21,3 @@ toggleCheckbox.addEventListener('change', () => {
         });
     });
 });
-=======
-const toggleCheckbox = document.getElementById('previewToggle');
-
-// Load current state
-chrome.storage.sync.get('previewsEnabled', (res) => {
-    toggleCheckbox.checked = res.previewsEnabled ?? true;
-});
-
-toggleCheckbox.addEventListener('change', () => {
-    const newVal = toggleCheckbox.checked;
-
-    // Save new setting
-    chrome.storage.sync.set({ previewsEnabled: newVal }, () => {
-        // Send message to active tab
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            if (tabs[0]?.id) {
-                chrome.tabs.sendMessage(tabs[0].id, {
-                    type: 'previewToggleChanged',
-                    value: newVal
-                });
-            }
-        });
-    });
-});
->>>>>>> 00457b1babde9906d997b001847d58b146ec23ff
